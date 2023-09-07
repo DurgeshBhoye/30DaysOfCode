48b5e1650ffd75f17b8c2d3b10a25f3a1dd981f6
Durgesh Bhoye <117005843+DurgeshBhoye@users.noreply.github.com>
2023-09-07 20:54:39 +0530

    #Day 5

--- Day 5/SortAnArray.py
+++ Day 5/SortAnArray.py
@@ -1,5 +1,19 @@
-# python code
-
+# sort in ascending order 
 arr = [10, 2, 5, 3, 1]
 arr.sort()
 print(arr)
+
+
+
+# sort in descending order 
+def sortArrDesc(arr):
+"""Sorts an array in descending order."""
+  for i in range(len(arr)):
+    for j in range(i + 1, len(arr)):
+      if arr[i] < arr[j]:
+        arr[i], arr[j] = arr[j], arr[i]
+
+
+arr = [10, 1, 5, 3, 2]
+sortArrDesc(array)
+print(array)
